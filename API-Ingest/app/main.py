4155b8e31019c6475b48de955ec55561525c2415
Daniel Gomez Perry <danielrgomez@gmail.com>
2024-03-19 18:03:58 -0400

    modified:   API-Ingest/app/__pycache__/main.cpython-310.pyc 	modified:   API-Ingest/app/main.py

--- API-Ingest/app/main.py
+++ API-Ingest/app/main.py
@@ -60,7 +60,7 @@ async def post_bank_client(bankclient:BankClient):
 
 def produce_kafka_string(json_as_string):
         # Create producer
-        producer = KafkaProducer(bootstrap_servers='localhost:9093',acks=1)
+        producer = KafkaProducer(bootstrap_servers='kafka:9092',acks=1)
         
         # Writes the string as bytes because Kafka needs it this way
         producer.send('ingestion-topic', bytes(json_as_string, 'utf-8'))
